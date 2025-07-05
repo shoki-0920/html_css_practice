b746ae8dec03c25d63e488731a1cb0cd33b59b0c
shoki-0920 <ueharattyo920@gmail.com>
2025-07-05 16:55:08 +0900

    chapter4

--- chapter4/09-practice-base/script.js
+++ chapter4/09-practice-base/script.js
@@ -2,3 +2,14 @@
 - 猫 35%、犬 30%、きりん 20%、その他 15%
 - 円グラフのオプションには 幅100%、高さ300px を指定
 */
+var pieData = {
+    labels: ['cat', 'dog'],
+    series: [65, 35]
+};
+
+var pieOptions = {
+    with: '100%',
+    height: '300px'
+};
+
+new Chartist.Pie('.animal-chart', pieData, pieOptions);
